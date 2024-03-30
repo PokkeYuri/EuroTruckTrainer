df51f78a36d5a278d3815caa70ef309d2630b609
PokkeYuri <86960788+PokkeYuri@users.noreply.github.com>
2024-03-30 21:19:49 +0100

    Add project files.

---
+++ EuroTruckTrainer/EuroTrainer.cs
@@ -0,0 +1,103 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace EuroTruckTrainer
+{
+    internal class EuroTrainer
+    {
+        private const int PROCESS_ALL_ACCESS = 0x1F0FFF;
+
+        private IntPtr moneyPointer;
+
+        private Process Process;
+        private IntPtr processHandle;
+
+        public uint Money { get; private set; } = 0;
+
+        public EuroTrainer()
+        {
+            SetProcess();
+            SetMoneyAdressPointer();
+            GetMoney();
+        }
+
+        private void SetProcess()
+        {
+            Process = Process.GetProcessesByName("eurotrucks2")[0];
+            processHandle = Kernel32.OpenProcess(PROCESS_ALL_ACCESS, false, Process.Id);
+        }
+
+        private void SetMoneyAdressPointer()
+        {
+            IntPtr moduleBaseAddress = Process!.MainModule!.BaseAddress; // The module base address is the starting point for the pointer chain.
+
+            IntPtr pointer = IntPtr.Add(moduleBaseAddress, 0x01D079E8); // BASE_OFFSET
+
+            int[] offsets = [0x10, 0]; // Offsets
+
+            // Reading the pointer chain.
+            for (int i = 0; i < offsets.Length; i++)
+            {
+                byte[] buffer = new byte[8]; // Use 8 bytes for 64-bit addresses.
+                if (!Kernel32.ReadProcessMemory(processHandle, pointer, buffer, buffer.Length, out _))
+                {
+                    Console.WriteLine($"Failed to read from address: {pointer.ToString("X")}");
+                    return;
+                }
+
+                pointer = (IntPtr)(long)BitConverter.ToUInt64(buffer, 0);
+                pointer = IntPtr.Add(pointer, offsets[i]);
+            }
+
+            moneyPointer = IntPtr.Add(pointer, 0x10); //final Offset
+        }
+
+        public bool GetMoney()
+        {
+            // Assuming the value at the final pointer is a 4-byte integer.
+            byte[] valueBuffer = new byte[4];
+            if (!Kernel32.ReadProcessMemory(processHandle, moneyPointer, valueBuffer, valueBuffer.Length, out _))
+            {
+                Console.WriteLine($"Failed to read from final address: {moneyPointer.ToString("X")}");
+                return false;
+            }
+
+            Money = BitConverter.ToUInt32(valueBuffer, 0);
+            Console.WriteLine($"Current value: {Money}");
+            return true;
+        }
+
+        public bool SetMoney(uint newValue)
+        {
+            if(newValue > uint.MaxValue || newValue < uint.MinValue)
+            {
+                Console.WriteLine("Value out of border.");
+                return false;
+            }
+
+            byte[] newValueBuffer = BitConverter.GetBytes(newValue);
+            if (!Kernel32.WriteProcessMemory(processHandle, moneyPointer, newValueBuffer, newValueBuffer.Length, out _))
+            {
+                Console.WriteLine($"Failed to write to address: {moneyPointer.ToString("X")}");
+                return false;
+            }
+
+            Console.WriteLine($"New value written: {Money}");
+            return true;
+        }
+
+        public bool SetMinusOneForNegativeBalance() => SetMoney(uint.MaxValue);
+    }
+
+    internal static class Kernel32
+    {
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int dwSize, out int lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int nSize, out int lpNumberOfBytesWritten);
+    }
+}
---
+++ EuroTruckTrainer/MainWindow.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System.Windows;
+using System.Globalization;
+
+namespace EuroTruckTrainer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static string version = "v1.0.0, Developed on ETS2 Version \"1.49.2.23s(64-bit)\"";
+
+
+        public bool IsConnected => trainer != null;
+
+        private EuroTrainer? trainer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            txtVersion.Text = version;
+            btnNegBalance.IsEnabled = false;
+            SetBtnVisibility();
+        }
+
+        private uint _number;
+        public uint Number
+        {
+            get { return _number; }
+            set
+            {
+                try
+                {
+                    trainer?.SetMoney(value);
+                    trainer?.GetMoney();
+
+                    _number = trainer!.Money;
+                    txtNum.Text = _number.ToString("N", CultureInfo.CurrentCulture) + "€";
+                    SetBtnVisibility();
+                }
+                catch (Exception ex)
+                {
+                    ThrowError(ex);
+                }
+            }
+        }
+
+        private void BtnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (IsConnected) return;
+                trainer = new EuroTrainer();
+                SetBtnVisibility();
+            }
+            catch
+            {
+                MessageBox.Show(this, "Game not found. Please start the game and try again.", "Error");
+            }
+            StartUpdateProcessAsync().ConfigureAwait(false);
+        }
+
+        private async Task StartUpdateProcessAsync()
+        {
+            try
+            {
+                while (true)
+                {
+                    if (IsConnected)
+                    {
+                        Dispatcher.Invoke(() =>
+                        {
+                            trainer?.GetMoney();
+                            _number = trainer!.Money;
+                            txtNum.Text = _number.ToString("N", CultureInfo.CurrentCulture) + "€";
+                            SetBtnVisibility();
+                        });
+                    }
+                    
+                    await Task.Delay(1000);
+                }
+            }
+            catch (Exception ex)
+            {
+                ThrowError(ex);
+            }
+        }
+
+        private void ThrowError(Exception ex)
+        {
+            trainer = null;
+            SetBtnVisibility();
+
+            string errormsg = "Error on updating Value. Programm is closing!";
+            if (ex != null) errormsg += "\n\n" + ex.Message;
+
+            MessageBox.Show(this, errormsg, "Error");
+            Environment.Exit(0);
+        }
+
+        private void btnNegBalance_Click(object sender, RoutedEventArgs e)
+        {
+            var ok = MessageBox.Show(this, "This feature is available only when your balance is negative, setting it to -1€. To use this tool normally again, you can secure an in-game loan to return your balance to positive.\n\nDo you wish to proceed?", "EuroTruck Trainer",MessageBoxButton.OKCancel);
+            if (ok == MessageBoxResult.OK)
+            {
+                try
+                {
+                    trainer?.SetMinusOneForNegativeBalance();
+                }
+                catch (Exception ex)
+                {
+                    ThrowError(ex);
+                }
+                
+            }
+        }
+        private void SetBtnVisibility()
+        {
+            long number = _number;
+            btnMinus100.IsEnabled = IsConnected && (number - 100) >= 0 ? true : false;
+            btnMinus1_000.IsEnabled = IsConnected && (number - 1_000) >= 0 ? true : false;
+            btnMinus10_000.IsEnabled = IsConnected && (number - 10_000) >= 0 ? true : false;
+            btnMinus100_000.IsEnabled = IsConnected && (number - 100_000) >= 0 ? true : false;
+
+            btnPlus100.IsEnabled = IsConnected ? true : false;
+            btnPlus1_000.IsEnabled = IsConnected ? true : false;
+            btnPlus10_000.IsEnabled = IsConnected ? true : false;
+            btnPlus100_000.IsEnabled = IsConnected ? true : false;
+
+            btnNegBalance.IsEnabled = IsConnected;
+            btnConnect.IsEnabled = !IsConnected;
+        }
+
+        private void Increase100_Click(object sender, RoutedEventArgs e) => Number += 100;
+        private void Increase1000_Click(object sender, RoutedEventArgs e) => Number += 1000;
+        private void Increase10000_Click(object sender, RoutedEventArgs e) => Number += 10000;
+        private void Increase100000_Click(object sender, RoutedEventArgs e) => Number += 100000;
+        private void Decrease100_Click(object sender, RoutedEventArgs e) => Number -= 100;
+        private void Decrease1000_Click(object sender, RoutedEventArgs e) => Number -= 1000;
+        private void Decrease10000_Click(object sender, RoutedEventArgs e) => Number -= 10000;
+        private void Decrease100000_Click(object sender, RoutedEventArgs e) => Number -= 100000;
+
+        private void txtVersion_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
